ca4ca3e461553e6c60626888b851e40e3faa1052
vozoromnotna <anton20012010@gmail.com>
2022-07-10 15:31:45 +0300

    Initial commit

---
+++ DevEQ/DevEQ_Model.cs
@@ -0,0 +1,141 @@
+﻿
+using AO_Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AO_Lib.AO_Devices;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MathNet.Numerics.Interpolation;
+
+namespace DevEQ
+{
+    public class DevEQ_Model : INotifyPropertyChanged
+    {
+        public AO_Lib.AO_Devices.AO_Filter Filter;
+
+        public DevEQ_Model()
+        {
+            X.CollectionChanged += CollectionChanged;
+            Y.CollectionChanged += CollectionChanged;
+        }
+
+        private void CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Interpolator = MathNet.Numerics.Interpolate.CubicSpline(X, Y);
+        }
+
+        private ObservableCollection<double> Hzs; //хранит информацию о том, какие строчки записаны в исходном файле
+        private ObservableCollection<double> Wls;
+        private ObservableCollection<double> Intens;
+        private IInterpolation Interpolator;
+
+        private int points_count = 10;
+        public int PointsCount
+        {
+            get { return points_count; }
+            set { points_count = value; }
+        }
+
+        private double minX;
+        private double maxX;
+        private double minY;
+        private double maxY;
+
+        
+        public ObservableCollection<double> X;
+
+        public ObservableCollection<double> Y;
+        
+
+        private string dev_path;
+        public string DevPath {
+            get
+            {
+                return dev_path;
+            }
+            set
+            {
+                dev_path = value;
+                try
+                {
+                    var Status = Filter.Read_dev_file(dev_path);
+                    if (Status != 0)
+                        throw new Exception(Filter.Implement_Error(Status)); 
+
+                }
+                catch (Exception exc)
+                {
+                    throw new Exception(exc.Message);
+                }
+
+                ReadDevStrings();
+
+                IntializateInterpolator();
+
+            } 
+        }
+
+        private void IntializateInterpolator()
+        {
+            Interpolator = MathNet.Numerics.Interpolate.CubicSpline(Hzs, Intens);
+            minX = Hzs.Min();
+            maxX = Hzs.Max();
+            minY = Intens.Min();
+            maxY = Intens.Max();
+            X = new ObservableCollection<double>();
+            Y = new ObservableCollection<double>();
+            var stepX = (maxX - minX)/((double)PointsCount);
+            for (double i = minX; i <= maxX; i+= stepX)
+            {
+                X.Add(i);
+                Y.Add(Interpolator.Interpolate(i));
+            }
+        }
+
+        private void UpdateData()
+        {
+            Interpolator = MathNet.Numerics.Interpolate.CubicSpline(X, Y);
+            for (int i = 0; i < Hzs.Count; i++)
+            {
+
+            }
+        }
+
+        private void ReadDevStrings()
+        {
+            var Data_from_dev = Helper.Files.Read_txt(dev_path);
+            string[] AllStrings = Data_from_dev.ToArray();
+            float[] Params = new float[3];
+            Hzs = new ObservableCollection<double>();
+            Wls = new ObservableCollection<double>();
+            Intens = new ObservableCollection<double>();
+            for (int i = 0; i < Data_from_dev.ToArray().Length; i++)
+            {
+                try
+                {
+                    Helper.Files.Get_WLData_fromDevString(AllStrings[i], 3, Params);
+                    Hzs.Add(Params[0]); Wls.Add(Params[1]); Intens.Add(Params[2]);
+                }
+                catch
+                {
+                    continue;
+                }
+            }
+            Hzs.Reverse();
+            Wls.Reverse();
+            Intens.Reverse();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ DevEQ/DevEQ_ViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using Microsoft.Win32;
+
+
+namespace DevEQ
+{
+    public class DevEQ_ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<string> Logs
+        {
+            get;
+            set;
+        }
+
+        public void Message(string message)
+        {
+            if (null == message)
+            {
+                throw new ArgumentNullException("message");
+            }
+            string data = string.Format("{0:yyyy-MM-dd HH:mm:ss.fff}: {1}", DateTime.Now, message);
+            Logs.Add(data);
+        }
+        private ChartValues<ObservablePoint> points;
+        public ChartValues<ObservablePoint> Points
+        {
+            get { return points; }
+            set { points = value; OnPropertyChanged(); }
+        }
+
+        public DevEQ_ViewModel()
+        {
+            Logs = new ObservableCollection<string>();
+            Message(" - текущее время");
+            this.MainModel = new DevEQ_Model();
+            var Filters = AO_Lib.AO_Devices.AO_Filter.Find_all_filters();
+            if (Filters.Count > 0)
+            {
+                if (Filters.Count > 1) Message("Обнаружено несколько фильтров. Будет подключен первый обнаруженный...");
+                MainModel.Filter = Filters[0];
+            }
+            else if (Filters.Count == 0)
+            {
+                Message("Не обнаружены именованные фильтры. Поиск неименнованного фильтра...");
+                MainModel.Filter = AO_Lib.AO_Devices.AO_Filter.Find_and_connect_AnyFilter();
+            }
+
+            if (MainModel.Filter.FilterType == AO_Lib.AO_Devices.FilterTypes.Emulator) { Message("ПРЕДУПРЕЖДЕНИЕ: Не обнаружены подключенные АО фильтры. Фильтр будет эмулирован."); }
+            else { Message("Обнаружен подключенный АО фильтр. Тип фильтра: " + MainModel.Filter.FilterType.ToString()); }
+
+            Points = new ChartValues<ObservablePoint>();
+
+        }
+
+        public DevEQ_Model MainModel { get; private set; }
+
+        #region Комманды
+        private RelayCommand open_dev;
+        public RelayCommand OpenDev
+        {
+            get
+            {
+                return open_dev ??
+                  (open_dev = new RelayCommand(obj =>
+                  {
+                      OpenFileDialog OPF = new OpenFileDialog();
+                      OPF.Filter = "DEV config files (*.dev)|*.dev|All files (*.*)|*.*";
+                      OPF.FilterIndex = 0;
+                      OPF.RestoreDirectory = true;
+
+                      if (OPF.ShowDialog() == true)
+                      {
+                          MainModel.DevPath = OPF.FileName;
+                          Message(MainModel.DevPath + " - файл считан успешно!");
+                      }
+
+                      Points.Clear();
+                      for (int i = 0; i < MainModel.X.Count; i++)
+                      {
+                          Points.Add(new ObservablePoint(MainModel.X[i], MainModel.Y[i]));
+                          Points[i].PropertyChanged += (s, e) => { MainModel.X[i] = Points[i].X; MainModel.Y[i] = Points[i].Y; };
+                      }
+                  }));
+            }
+        }
+        #endregion
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
